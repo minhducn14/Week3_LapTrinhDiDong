ba554b947f04a48fec3bc63658cfa97d6e46bf78
unknown <vunguyenminhduc1410@gmail.com>
2024-09-21 20:23:57 +0700

    Hoan thanh cau 1c

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import React from 'react';
 import FirstScreen from './screens/FirstScreen';
 import Cau1a from './screens/Cau1a';
 import Cau1b from './screens/Cau1b';
+import Cau1c from './screens/Cau1c';
 export default function App() {
   return (
     // <FirstScreen />
     // <Cau1a />
-    <Cau1b />
+    // <Cau1b />
+    <Cau1c />
   );
 };
